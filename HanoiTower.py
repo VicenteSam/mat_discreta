f3baae45b8ff10355a3e3080ea6987cdc954d7e3
Vicente Sampaio <141889498+VicenteSam@users.noreply.github.com>
2024-09-20 11:49:01 -0400

    Update HanoiTower.py

--- HanoiTower.py
+++ HanoiTower.py
@@ -4,7 +4,7 @@ class hanoiTower:
         self.numeroDeDiscos = 0
         self.numeroDeMovimentos = 0
         self.movimentos_binarios = []
-
+    # Getters e Setters
     def getNumeroDeDiscos(self):
         return self.numeroDeDiscos
     
@@ -16,13 +16,13 @@ class hanoiTower:
     
     def setNumeroDeMovimentos(self, numeroDeMovimentos):
         self.numeroDeMovimentos = numeroDeMovimentos
-
+    # Recebe número de discos (máx. 5)
     def inputQtd(self):
         self.numeroDeDiscos = int(input("Número de discos (máximo 5): "))
         if self.numeroDeDiscos > 5:
             print("Número máximo de discos é 5.")
             self.numeroDeDiscos = 5
-
+    # Faz as trocas entre as torres
     def trocas(self, numDiscos, A, B, C):
         if numDiscos == 1:
             self.setNumeroDeMovimentos(self.getNumeroDeMovimentos()+1)
@@ -36,15 +36,15 @@ class hanoiTower:
             print(f"Movimentos: {self.getNumeroDeMovimentos()}\n")
             self.atualiza_matriz_binaria(numDiscos)
             self.trocas(numDiscos-1, B, A, C)
-
+    # Atualiza a matriz
     def atualiza_matriz_binaria(self, disco_movido):
         linha_movimento = [0] * self.numeroDeDiscos
         linha_movimento[disco_movido - 1] = 1
         self.movimentos_binarios.append(linha_movimento)
-
+    # Retorna total de movimentos
     def totalMovimentos(self):
         print(f"Total de movimentos: {self.getNumeroDeMovimentos()}\n")
-
+    # Retorna matriz binária
     def exibir_matriz_binaria(self):
         print("\nMatriz Binária de Movimentos:")
         for linha in self.movimentos_binarios:
